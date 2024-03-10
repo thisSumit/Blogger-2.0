f998ee7aca2909ead6cdecb1c0b8465b03f64b58
Sumit Karanjekar <122661393+thisSumit@users.noreply.github.com>
2024-03-10 15:17:37 +0530

    Update IndexPage.js

--- Frontend/src/pages/IndexPage.js
+++ Frontend/src/pages/IndexPage.js
@@ -4,7 +4,7 @@ import {useEffect, useState} from "react";
 export default function IndexPage() {
   const [posts,setPosts] = useState([]);
   useEffect(() => {
-    fetch('https://blogger-api-brown.vercel.app/post').then(response => {
+    fetch('https://mernblog-api-delta.vercel.app/post').then(response => {
       response.json().then(posts => {
         setPosts(posts);
       });
