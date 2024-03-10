fc0ece2d4260de2523a58b78323a3baf0228f7f4
Sumit Karanjekar <122661393+thisSumit@users.noreply.github.com>
2024-03-10 15:18:15 +0530

    Update LoginPage.js

--- Frontend/src/pages/LoginPage.js
+++ Frontend/src/pages/LoginPage.js
@@ -9,7 +9,7 @@ export default function LoginPage() {
   const {setUserInfo} = useContext(UserContext);
   async function login(ev) {
     ev.preventDefault();
-    const response = await fetch('https://blogger-api-brown.vercel.app/login', {
+    const response = await fetch('https://mernblog-api-delta.vercel.app/login', {
       method: 'POST',
       body: JSON.stringify({username, password}),
       headers: {'Content-Type':'application/json'},
