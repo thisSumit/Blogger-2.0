3309b37cdcbc7de8a91b5a6157d5ba4afb047729
Sumit Karanjekar <122661393+thisSumit@users.noreply.github.com>
2024-03-10 15:14:24 +0530

    Update Header.js

--- Frontend/src/Header.js
+++ Frontend/src/Header.js
@@ -6,7 +6,7 @@ import { FaEdit } from "react-icons/fa";
 export default function Header() {
   const {setUserInfo,userInfo} = useContext(UserContext);
   useEffect(() => {
-    fetch('https://blogger-api-brown.vercel.app/profile', {
+    fetch('https://mernblog-api-delta.vercel.app/profile', {
       credentials: 'include',
     }).then(response => {
       response.json().then(userInfo => {
@@ -16,7 +16,7 @@ export default function Header() {
   }, []);
 
   function logout() {
-    fetch('https://blogger-api-brown.vercel.app/logout', {
+    fetch('https://mernblog-api-delta.vercel.app/logout', {
       credentials: 'include',
       method: 'POST',
     });
