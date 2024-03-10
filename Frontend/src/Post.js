d6f6728493ad9ad58a28d4d4da0b010c07e3d034
Sumit Karanjekar <122661393+thisSumit@users.noreply.github.com>
2024-03-10 15:14:57 +0530

    Update Post.js

--- Frontend/src/Post.js
+++ Frontend/src/Post.js
@@ -7,7 +7,7 @@ export default function Post({_id,title,summary,cover,content,createdAt,author})
     <div className="post">
       <div className="image">
         <Link to={`/post/${_id}`}>
-          <img src={'https://blogger-api-brown.vercel.app/'+cover} alt=""/>
+          <img src={'https://mernblog-api-delta.vercel.app/'+cover} alt=""/>
         </Link>
       </div>
       <div className="texts">
