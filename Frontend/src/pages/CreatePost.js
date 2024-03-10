f878efa9b00ad34fed62e61e2254a3123be69522
Sumit Karanjekar <122661393+thisSumit@users.noreply.github.com>
2024-03-10 15:16:14 +0530

    Update CreatePost.js

--- Frontend/src/pages/CreatePost.js
+++ Frontend/src/pages/CreatePost.js
@@ -17,7 +17,7 @@ export default function CreatePost() {
     data.set('content', content);
     data.set('file', files[0]);
     ev.preventDefault();
-    const response = await fetch('https://blogger-api-brown.vercel.app/post', {
+    const response = await fetch('https://mernblog-api-delta.vercel.app/post', {
       method: 'POST',
       body: data,
       credentials: 'include',
